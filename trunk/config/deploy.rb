e498901a8ebe257b421b3045b65e6b1c5fd61a54
ralphvdh <ralphvdh@e9429159-754f-0410-80d2-f1ba3b164796>
2008-06-26 17:55:28 +0000

    clean up

--- trunk/config/deploy.rb
+++ trunk/config/deploy.rb
@@ -1,16 +1,19 @@
 set :application, "wiwijobs"
 set :repository,  "https://wiwijobs.googlecode.com/svn/trunk"
+set :deploy_to, "/root/#{application}"
+
+ssh_options[:username] = 'root'
+set :password, "!DfghMnB456*"
+set :use_sudo, false
+
+set :scm, "subversion"
 set :scm_username, "sebastian.tuke"
 
-set :deploy_to, "/root/#{application}"
 
 role :app, "128.176.158.70"
 role :web, "128.176.158.70"
 role :db,  "128.176.158.70", :primary => true
 
-ssh_options[:username] = 'root'
-
-set :use_sudo, false
 
 desc "Webserver auf der WI-VM neu starten"
 task :restart_web_server, :roles => :web do
